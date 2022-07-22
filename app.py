ce4e2c62ad608afcd39d9eaf2647c345c5910c53
letsgopats678 <llany.joe6@gmail.com>
2022-07-22 16:35:30 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def players():  # put application's code here
+    return render_template("players.html")
+
+
+if __name__ == '__main__':
+    app.run()
